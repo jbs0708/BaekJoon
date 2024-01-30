e0625af90ba72221e61911677655ff389f058f95
jbs0708 <87466056+jbs0708@users.noreply.github.com>
2024-01-30 11:47:52 +0900

    [D1] Title: 최대 최소의 간격, Time: 157 ms, Memory: 58,496 KB -BaekjoonHub

---
+++ SWEA/D1/11092. 최대 최소의 간격/최대 최소의 간격.py
@@ -0,0 +1,22 @@
+T = int(input())
+
+for tc in range(1, T+1):
+    N = int(input())
+    list_ = list(map(int, input().split()))
+
+    min_id = N+1
+    min_val = 11
+    max_id = -1
+    max_val = 0
+
+    for n in range(N):
+        if list_[n] < min_val:
+            min_id = n
+            min_val = list_[n]
+        if list_[n] >= max_val:
+            max_id = n
+            max_val = list_[n]
+
+    ans = abs(max_id - min_id)
+
+    print(f'#{tc} {ans}')
