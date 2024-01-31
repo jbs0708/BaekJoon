24a5c8f41a793ea5e19f379db4c85df640e0687e
jbs0708 <87466056+jbs0708@users.noreply.github.com>
2024-01-31 09:22:26 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 230 ms, Memory: 63,300 KB -BaekjoonHub

---
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
@@ -0,0 +1,22 @@
+T = 10
+N = 100
+
+for tc in range(1, T+1):
+    n = input()
+    row = [0] * 100
+    col = [0] * 100
+    lc = 0
+    rc = 0
+
+    for r in range(N):
+        temp = list(map(int, input().split()))
+        for c in range(N):
+            row[r] += temp[c]
+            col[c] += temp[c]
+            if r == c:
+                rc += temp[c]
+            if (r + c) == (N-1):
+                lc += temp[c]
+
+    ans = max(max(row), max(col), lc, rc)
+    print(f'#{tc} {ans}')
