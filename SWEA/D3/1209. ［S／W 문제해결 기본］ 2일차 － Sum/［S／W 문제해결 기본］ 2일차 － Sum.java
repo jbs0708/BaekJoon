1ccf6c94f8d33b28d67d0078acf548a396847b66
jbs0708 <87466056+jbs0708@users.noreply.github.com>
2024-01-31 09:22:12 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 221 ms, Memory: 35,020 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
@@ -1,22 +1,47 @@
-T = 10
-N = 100
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
-for tc in range(1, T+1):
-    n = input()
-    row = [0] * 100
-    col = [0] * 100
-    lc = 0
-    rc = 0
+public class Solution {
 
-    for r in range(N):
-        temp = list(map(int, input().split()))
-        for c in range(N):
-            row[r] += temp[c]
-            col[c] += temp[c]
-            if r == c:
-                rc += temp[c]
-            if (r + c) == (N-1):
-                lc += temp[c]
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    ans = max(max(row), max(col), lc, rc)
-    print(f'#{tc} {ans}')
+        for (int T = 1; T <= 10; T++) {
+            int[][] arr = new int[100][100];
+            int rowSum[] = new int[100];
+            int colSum[] = new int[100];
+            int t = Integer.parseInt(sc.nextLine());
+            int arrMax = Integer.MIN_VALUE;
+            int diaMax = Integer.MIN_VALUE;
+            int max = Integer.MIN_VALUE;
+            int diagonal1 = 0;  // 대각선1
+            int diagonal2 = 0;  // 대각선2
+
+            for (int i = 0; i < 100; i++) {
+                int rowTemp = 0;
+                StringTokenizer st = new StringTokenizer(sc.nextLine()," ");
+                for (int j = 0; j < 100; j++) {
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                    colSum[j] += arr[i][j];
+                    rowTemp += arr[i][j];
+                    if (i == j) {
+                        diagonal1 += arr[i][j];
+                    }
+                    if ((i+j) == 99) {
+                        diagonal2 += arr[i][j];
+                    }
+                }
+                rowSum[i] = rowTemp;
+            }
+            Arrays.sort(colSum);  // 오름차순 정리
+            Arrays.sort(rowSum);  // 오름차순 정리
+
+            arrMax = Math.max(rowSum[99],colSum[99]);  // 행,열 최댓값 비교
+            diaMax = Math.max(diagonal1,diagonal2);    // 대각선 두개 최댓값 비교
+            max = Math.max(diaMax,arrMax);             // 최종 최댓값 비교
+
+            System.out.printf("#%d %d\n",t,max);
+        }
+    }
+}
