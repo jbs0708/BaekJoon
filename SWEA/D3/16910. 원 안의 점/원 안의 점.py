ef8a5bd0b959918e755a045462c2b3121abf2971
jbs0708 <87466056+jbs0708@users.noreply.github.com>
2024-02-02 09:15:07 +0900

    [D3] Title: 원 안의 점, Time: 286 ms, Memory: 61,448 KB -BaekjoonHub

---
+++ SWEA/D3/16910. 원 안의 점/원 안의 점.py
@@ -0,0 +1,12 @@
+T = int(input())
+
+for tc in range(1, T + 1):
+    N = int(input())
+
+    cnt = 0
+    for i in range(-N, N+1):
+        for j in range(-N, N+1):
+            if (i*i) + (j*j) <= (N*N):
+                cnt += 1
+
+    print(f'#{tc} {cnt}')
