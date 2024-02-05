266b63048d24f97d4781a6aff21cd5e3427270be
jbs0708 <87466056+jbs0708@users.noreply.github.com>
2024-02-05 15:32:58 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - GNS, Time: 536 ms, Memory: 73,852 KB -BaekjoonHub

--- SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.py
+++ SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.py
@@ -1,56 +1,16 @@
+seq_list = ["ZRO", "ONE", "TWO", "THR", "FOR", "FIV", "SIX", "SVN", "EGT", "NIN"]
+
 T = int(input())
-
-for tc in range(1, T+1):
-    temp = list(map(str, input().split()))
-    N = int(temp[1])
-
+for tc in range(1, T + 1):
+    t, N = map(str, input().split())
     list_ = list(map(str, input().split()))
-    ans_list = []
+    num_list = [0] * 10
+    result = []
 
-    for s in list_:
-        if s == 'ZRO':
-            ans_list.append(0)
-        elif s == 'ONE':
-            ans_list.append(1)
-        elif s == 'TWO':
-            ans_list.append(2)
-        elif s == 'THR':
-            ans_list.append(3)
-        elif s == 'FOR':
-            ans_list.append(4)
-        elif s == 'FIV':
-            ans_list.append(5)
-        elif s == 'SIX':
-            ans_list.append(6)
-        elif s == 'SVN':
-            ans_list.append(7)
-        elif s == 'EGT':
-            ans_list.append(8)
-        elif s == 'NIN':
-            ans_list.append(9)
-
-    ans_list.sort()
+    for i in list_:
+        num_list[seq_list.index(i)] += 1
 
     print(f'#{tc}')
-    for i in range(N):
-        if ans_list[i] == 0:
-            print('ZRO', end=' ')
-        elif ans_list[i] == 1:
-            print('ONE', end=' ')
-        elif ans_list[i] == 2:
-            print('TWO', end=' ')
-        elif ans_list[i] == 3:
-            print('THR', end=' ')
-        elif ans_list[i] == 4:
-            print('FOR', end=' ')
-        elif ans_list[i] == 5:
-            print('FIV', end=' ')
-        elif ans_list[i] == 6:
-            print('SIX', end=' ')
-        elif ans_list[i] == 7:
-            print('SVN', end=' ')
-        elif ans_list[i] == 8:
-            print('EGT', end=' ')
-        elif ans_list[i] == 9:
-            print('NIN', end=' ')
+    for i in range(len(num_list)):
+        print((seq_list[i] + ' ') * num_list[i], end='')
     print()
