ca9d154921699cc0d41064123b20e999b028a7e6
jbs0708 <87466056+jbs0708@users.noreply.github.com>
2024-02-05 12:35:44 +0900

    [D2] Title: 초심자의 회문 검사, Time: 157 ms, Memory: 58,268 KB -BaekjoonHub

---
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.py
@@ -0,0 +1,16 @@
+T = int(input())
+
+for tc in range(1, T+1):
+    input_str = input()
+    N = len(input_str)
+
+    found = True
+    for n in range(N//2):
+        if input_str[n] != input_str[(N - 1) - n]:
+            found = False
+            break
+
+    if found:
+        print(f'#{tc} 1')
+    else:
+        print(f'#{tc} 0')
