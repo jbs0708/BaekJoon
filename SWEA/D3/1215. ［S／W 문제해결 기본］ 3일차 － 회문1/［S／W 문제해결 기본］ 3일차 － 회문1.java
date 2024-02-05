d2ff423152201dc7b61b5d80d20d5c6a3daf1db8
jbs0708 <87466056+jbs0708@users.noreply.github.com>
2024-02-05 12:12:40 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문1, Time: 110 ms, Memory: 19,724 KB -BaekjoonHub

--- SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.java
+++ SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.java
@@ -1,29 +1,112 @@
-T = 10
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-for tc in range(1, T+1):
-    N = int(input())
+public class Solution {
 
-    list_ = [list(input()) for _ in range(8)]
+    static int cnt;
+    static char arr[][];
+    static int n;
 
-    cnt = 0
-    for y in range(8):
-        for x in range(8 - N + 1):
-            found = True
-            for n in range(N//2):
-                if list_[y][x+n] != list_[y][x+(N-1)-n]:
-                    found = False
-                    break
-            if found:
-                cnt += 1
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    for x in range(8):
-        for y in range(8 - N + 1):
-            found = True
-            for n in range(N//2):
-                if list_[y+n][x] != list_[y+(N-1)-n][x]:
-                    found = False
-                    break
-            if found:
-                cnt += 1
+        for (int t = 1; t <= 10; t++) {
+            n = Integer.parseInt(br.readLine());
+            arr = new char[8][8];
+            cnt = 0;
 
-    print(f'#{tc} {cnt}')
+            for (int i = 0; i < 8; i++) {
+                String str = br.readLine();
+                char c[] = str.toCharArray();
+                for (int j = 0; j < 8; j++) {
+                    arr[i][j] = c[j];
+                }
+            }
+
+            // 길이가 1인 회문의 갯수는 판의 넓이와 같으므로 8*8 = 64 
+            if (n == 1) {
+                System.out.printf("#%d 64\n", t);
+                continue;
+            }
+
+            // 각 위치에 대해서 가로세로 방향으로 탐색
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    // 행
+                    row(i,j);
+                    // 열
+                    column(i,j);
+                }
+            }
+
+            System.out.printf("#%d %d\n", t, cnt);
+
+        }
+    }
+
+    // 세로방향
+    public static void column(int x, int y) {
+        int plus = y;
+        int minus = y;
+        // 회문 길이의 홀수짝수 판별 후, 짝수라면 비교할 위치에 +1
+        if (n%2 == 0) plus++;
+
+        // 회문 길이 측정
+        int count = 0;
+        
+        // 판의 범위에서 벗어나지 않도록 설정
+        while (plus < 8 && minus >= 0) {
+            // 회문이 성립하지 않는다면 반복문 탈출
+            if (arr[x][plus] != arr[x][minus]) break;
+
+            // 회문 길이 카운트
+            count++;
+            // 비교위치 조정 
+            plus++;
+            minus--;
+
+            // 주어진 길이만큼 회문이 성립하면 카운트 추가 후 반복문 탈출
+            if (n%2 == 1 && count == (n/2) + 1) {
+                cnt++;
+                break;
+            }
+            if (n%2 == 0 && count == (n/2)) {
+                cnt++;
+                break;
+            }
+        }
+    }
+
+    // 가로방향
+    public static void row(int x, int y) {
+        int plus = x;
+        int minus = x;
+        if (n%2 == 0) plus++;
+
+        // 회문 길이 측정
+        int count = 0;
+
+        // 판의 범위에서 벗어나지 않도록 설정
+        while (plus < 8 && minus >= 0) {
+            // 회문이 성립하지 않는다면 반복문 탈출
+            if (arr[plus][y] != arr[minus][y]) break;
+            
+            // 회문 길이 카운트
+            count++;
+            // 비교위치 조정 
+            plus++;
+            minus--;
+            
+            // 주어진 길이만큼 회문이 성립하면 카운트 추가 후 반복문 탈출
+            if (n%2 == 1 && count == (n/2) + 1) {
+                cnt++;
+                break;
+            }
+            if (n%2 == 0 && count == (n/2)) {
+                cnt++;
+                break;
+            }
+        }
+    }
+}
