7369ffdc95d4ebb1c6dd812bf20fd9590987d853
jbs0708 <87466056+jbs0708@users.noreply.github.com>
2024-02-06 10:10:00 +0900

    [D2] Title: Gravity, Time: 151 ms, Memory: 58,512 KB -BaekjoonHub

---
+++ SWEA/D2/16504. Gravity/Gravity.py
@@ -0,0 +1,16 @@
+T = int(input())
+
+for tc in range(1, T+1):
+    N = int(input())
+    arr = list(map(int, input().split()))
+
+    max_v = 0
+    for i in range(N-1):
+        cnt = 0
+        for j in range(i+1, N):
+            if arr[i] > arr[j]:
+                cnt += 1
+        if max_v < cnt:
+            max_v = cnt
+
+    print(f'#{tc} {max_v}')
