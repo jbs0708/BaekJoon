f391ad9b9e1bd7409045f3295b0d2c71eafbfe63
jbs0708 <87466056+jbs0708@users.noreply.github.com>
2024-02-19 20:36:44 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -0,0 +1,23 @@
+N = int(input())
+matrix = [list(map(int, input().split())) for _ in range(N)]
+
+
+dp = [[[0 for _ in range(N)] for _ in range(N)] for _ in range(3)]
+
+dp[0][0][1] = 1
+
+for i in range(2, N):
+    if matrix[0][i] == 0:
+        dp[0][0][i] = dp[0][0][i-1]
+
+for i in range(1, N):
+    for j in range(1, N):
+        if matrix[i][j] == 0 and matrix[i][j - 1] == 0 and matrix[i-1][j] == 0:
+            dp[2][i][j] = dp[0][i-1][j-1] + dp[1][i-1][j-1] + dp[2][i-1][j-1]
+
+        if matrix[i][j] == 0:
+            dp[0][i][j] = dp[0][i][j-1] + dp[2][i][j-1]
+            dp[1][i][j] = dp[1][i-1][j] + dp[2][i-1][j]
+
+
+print(sum(dp[i][N-1][N-1] for i in range(3)))
