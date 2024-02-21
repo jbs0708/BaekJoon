96bd9efe58b9ec6c98b6e5875238c1ff6e779bad
jbs0708 <87466056+jbs0708@users.noreply.github.com>
2024-02-21 09:46:44 +0900

    [Gold IV] Title: DSLR, Time: 5092 ms, Memory: 214420 KB -BaekjoonHub

---
+++ 백준/Gold/9019. DSLR/DSLR.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+T = int(input())
+
+for tc in range(1, T + 1):
+    A, B = map(int, input().rstrip().split())
+    visited = [False for _ in range(10000)]
+    visited[A] = True
+
+    q = deque()
+    q.append((A, ''))
+
+    while q:
+        a, b = q.popleft()
+
+        if a == B:
+            print(b)
+            break
+
+        d = (a * 2) % 10000
+        if not visited[d]:
+            visited[d] = True
+            q.append((d, b + 'D'))
+
+        if a == 0:
+            s = 9999
+        else:
+            s = a - 1
+        if not visited[s]:
+            visited[s] = True
+            q.append((s, b + 'S'))
+
+        l = (a // 1000) + (a % 1000) * 10
+        if not visited[l]:
+            visited[l] = True
+            q.append((l, b + 'L'))
+
+        r = (a % 10) * 1000 + (a // 10)
+        if not visited[r]:
+            visited[r] = True
+            q.append((r, b + 'R'))
