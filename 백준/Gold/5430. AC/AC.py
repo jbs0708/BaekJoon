aa25e36cc042775a14eb071e8f9f8402bbd667ba
jbs0708 <87466056+jbs0708@users.noreply.github.com>
2024-03-14 11:34:54 +0900

    [Gold V] Title: AC, Time: 300 ms, Memory: 152600 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.py
@@ -0,0 +1,42 @@
+from collections import deque
+
+T = int(input())
+
+for tc in range(1, T+1):
+    P = input()
+    N = int(input())
+    inputStr = input()
+    inputStr = inputStr[1:len(inputStr)-1]
+    q = deque()
+    if len(inputStr) > 0:
+        str = list(map(int, inputStr.split(',')))
+        q = deque(str)
+
+    direction = True
+    error = False
+    for p in P:
+        if p == 'R':
+            direction = not direction
+        elif p == 'D':
+            if q:
+                if direction:
+                    q.popleft()
+                else:
+                    q.pop()
+            else:
+                error = True
+
+    if not error:
+        print('[', end='')
+        if q:
+            if direction:
+                print(q.popleft(), end='')
+                while q:
+                    print(f',{q.popleft()}', end='')
+            else:
+                print(q.pop(), end='')
+                while q:
+                    print(f',{q.pop()}', end='')
+        print(']')
+    else:
+        print('error')
