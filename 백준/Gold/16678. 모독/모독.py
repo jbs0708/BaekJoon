fec9c1e8253382bffacf24ce55b6fd7fb6486792
jbs0708 <87466056+jbs0708@users.noreply.github.com>
2024-05-03 16:24:33 +0900

    [Gold V] Title: 모독, Time: 196 ms, Memory: 114916 KB -BaekjoonHub

---
+++ 백준/Gold/16678. 모독/모독.py
@@ -0,0 +1,20 @@
+N = int(input())
+inp_list = []
+
+for i in range(N):
+    inp_list.append(int(input()))
+
+inp_list.sort()
+
+num = 1
+ans = 0
+for p in inp_list:
+    if p == num:
+        num += 1
+    elif p < num:
+        continue
+    else:
+        ans += (p-num)
+        num += 1
+
+print(ans)
