92512c10f47f50236410a4c5b50f991cd09f8c58
jbs0708 <87466056+jbs0708@users.noreply.github.com>
2024-07-23 20:37:26 +0900

    [Gold IV] Title: 미로만들기, Time: 144 ms, Memory: 15300 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.java
+++ 백준/Gold/2665. 미로만들기/미로만들기.java
@@ -1,59 +1,63 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Main {
-    static int map[][];
-    static int dx[] = {0,1,0,-1};
-    static int dy[] = {1,0,-1,0};
-    static int dist[][];
+
     static int N;
+    static int map[][];
+    static int dis[][];
+    static int[] dy = new int[]{0, 0, 1, -1};
+    static int[] dx = new int[]{1, -1, 0, 0};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+
         N = Integer.parseInt(br.readLine());
         map = new int[N][N];
-        dist = new int[N][N];
+        dis = new int[N][N];
 
-        for(int i=0;i<N;i++){
+        for (int i = 0; i < N; i++) {
             String str = br.readLine();
-            for(int j=0;j<N;j++){
-                map[i][j] = str.charAt(j)-'0';
-                dist[i][j] = Integer.MAX_VALUE;
+            for (int j = 0; j < N; j++) {
+                map[i][j] = str.charAt(j) - '0';
+                dis[i][j] = Integer.MAX_VALUE;
             }
         }
+
         bfs();
 
-
-        System.out.println(dist[N-1][N-1]);	//배열은 0부터 시작이므로 
+        System.out.println(dis[N-1][N-1]);
     }
-    public static void bfs(){
-        Queue<Node> q = new LinkedList<>();
-        q.add(new Node(0,0));   //시작점
-        dist[0][0] = 0;
 
-        while(!q.isEmpty()){
+    public static void bfs() {
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(0,0));
+        dis[0][0] = 0;
+
+        while (!q.isEmpty()) {
             Node now = q.poll();
 
-            for(int i=0;i<4;i++){   //상하좌우 탐색
-                int nextX = now.x+dx[i];
-                int nextY = now.y+dy[i];
+            for (int i = 0; i < 4; i++) {
+                int ny = now.y + dy[i];
+                int nx = now.x + dx[i];
 
-                if(nextX >=0 && nextY >= 0 && nextX < N && nextY < N){
-                    if(dist[nextX][nextY] > dist[now.x][now.y]){
-                        if(map[nextX][nextY] == 1) dist[nextX][nextY] = dist[now.x][now.y];
-                        else dist[nextX][nextY] = dist[now.x][now.y]+1; //이동
-                        q.add(new Node(nextX,nextY));
+                if (nx < N && ny < N && nx >= 0 && ny >= 0) {
+                    if (dis[nx][ny] > dis[now.x][now.y]) {
+                        if (map[nx][ny] == 1) {
+                            dis[nx][ny] = dis[now.x][now.y];
+                        } else {
+                            dis[nx][ny] = dis[now.x][now.y] + 1;
+                        }
+                        q.add(new Node(nx, ny));
                     }
                 }
             }
         }
     }
-
 
     public static class Node{
         int x;
